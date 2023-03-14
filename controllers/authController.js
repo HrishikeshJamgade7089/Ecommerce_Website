7b400d036b4d8db9809836aacb217aa6d5e78cdd
HrishikeshJamgade7089 <hrishikeshjamgade7089@gmail.com>
2023-03-14 22:45:57 +0530

    First commit

---
+++ controllers/authController.js
@@ -0,0 +1,50 @@
+import userModel from "../models/userModel.js"
+import { hashPassword } from "../helpers/authHelper.js"
+
+export const registerController = async(req,res)=>{
+    try {
+        const {name,email,password,phone,address} = req.body 
+        //validations
+        if(!name){
+            return res.send({error:'Name is Required'});
+        }
+        if(!email){
+            return res.send({error:'Name is Required'});
+        }
+        if(!password){
+            return res.send({error:'Name is Required'});
+        }
+        if(!address){
+            return res.send({error:'Name is Required'});
+        }
+
+        const existingUser=await userModel.findOne({email})
+
+        //existing user
+        if(existingUser){
+            return res.status(200).send({
+                success:true,
+                message:'Already Register please login',
+            })
+        }
+
+        //register user
+        const hashedPassword = await hashPassword(password)
+        //save
+        const user =await new userModel({name,email,phone,address,password:hashedPassword}).save()
+
+        res.status(201).send({
+            success:true,
+            message:'User Register Successfully',
+            user
+        })
+        
+    }catch(error){
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:'Error in Registration',
+            error
+        })
+    }
+}
